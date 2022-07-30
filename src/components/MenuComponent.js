39191a60ab4a95f72ff9dbca69e3e25105e73616
Nathani Luving <cosmasnathan9@gmail.com>
2022-07-30 18:51:59 +0300

    Add render function

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -15,6 +15,26 @@ class Menu extends Component{
         this.setState({selectedDish: dish});
     }
 
+    renderDish(dish){
+        if(dish != null){
+            return (
+                <Card>
+                    <CardImg width="100%" src={dish.image} alt={dish.name}/>
+                    <CardBody>
+                        <CardTitle>{dish.name}</CardTitle>
+                        <CardText>{dish.description}</CardText>
+                    </CardBody>
+                </Card>
+            );
+
+        }
+        else {
+            return (
+                <div></div>
+            )
+        }
+    }
+
     render(){
         const menu = this.props.dishes.map((dish) => {
             return (
@@ -34,6 +54,10 @@ class Menu extends Component{
             <div className="container">
                 <div className="row">
                     {menu}
+                </div>
+
+                <div className="row">
+                    {this.renderDish(this.state.selectedDish)}
                 </div>
             </div>
         );
