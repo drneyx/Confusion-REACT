55648aca0798e9bda7688c4c23ce31b48463dfc3
Nathani Luving <cosmasnathan9@gmail.com>
2022-10-13 14:22:33 +0300

    Add comments section

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -5,13 +5,22 @@ class DishDetail extends Component{
 
     render() {
         return (
-            <Card>
-                <CardImg width="100%" src={this.props.dish.image} alt={this.props.dish.name}/>
-                <CardBody>
-                    <CardTitle>{this.props.dish.name}</CardTitle>
-                    <CardText>{this.props.dish.description}</CardText>
-                </CardBody>
-            </Card>
+            <div className="row">
+                <div className="col-12 col-md-5 m-1">
+                    <Card>
+                        <CardImg width="100%" src={this.props.dish.image} alt={this.props.dish.name}/>
+                        <CardBody>
+                            <CardTitle>{this.props.dish.name}</CardTitle>
+                            <CardText>{this.props.dish.description}</CardText>
+                        </CardBody>
+                    </Card>
+                </div>
+
+                <div className="col-12 col-md-5 m-1">
+                    <h1>Comments here</h1>
+                </div>
+            </div>
+            
         )
     }
 }
