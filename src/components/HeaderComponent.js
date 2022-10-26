ceef74a2105243b1fffe48b2cbc3c0af04ec5a29
Nathani Luving <cosmasnathan9@gmail.com>
2022-10-26 19:21:56 +0300

    add toggle nav

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,40 +1,58 @@
 import React, { Component} from 'react';
-import { Navbar, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
+import { Navbar, NavbarBrand, Nav, NavItem, NavLink, Collapse, NavbarToggler } from 'reactstrap';
 
 class Header extends Component {
+
+
+    constructor(props){
+        super(props);
+        this.state = {
+            isNavOpen: false
+        };
+        this.toggleNav = this.toggleNav.bind(this);
+    }
+
+    toggleNav(){
+        this.setState({
+            isNavOpen: !this.state.isNavOpen
+        })
+    }
     render() {
         return (
             <>
                 <Navbar dark expand="md">
                     <div className="container">
+                        <NavbarToggler onClick={this.toggleNav}/>
                         <NavbarBrand className="mr-auto" href='/'>
                             <img src="assets/images/logo.png" height="30" width="41"alt="Nice Restaurant"/>
                         </NavbarBrand>
-                        <Nav navbar>
-                            <NavItem>
-                                <NavLink className="nav-link" to="/home">
-                                    <span className="fa fa-home fa-lg">Home</span>
-                                </NavLink>
-                            </NavItem>
+                        <Collapse isOpen={this.state.isNavOpen} navbar>
+                            <Nav navbar>
+                                <NavItem>
+                                    <NavLink className="nav-link" to="/home">
+                                        <span className="fa fa-home fa-lg">Home</span>
+                                    </NavLink>
+                                </NavItem>
 
-                            <NavItem>
-                                <NavLink className="nav-link" to="/aboutus">
-                                    <span className="fa fa-info fa-lg">About Us</span>
-                                </NavLink>
-                            </NavItem>
+                                <NavItem>
+                                    <NavLink className="nav-link" to="/aboutus">
+                                        <span className="fa fa-info fa-lg">About Us</span>
+                                    </NavLink>
+                                </NavItem>
 
-                            <NavItem>
-                                <NavLink className="nav-link" to="/menu">
-                                    <span className="fa fa-list fa-lg">Menu</span>
-                                </NavLink>
-                            </NavItem>
+                                <NavItem>
+                                    <NavLink className="nav-link" to="/menu">
+                                        <span className="fa fa-list fa-lg">Menu</span>
+                                    </NavLink>
+                                </NavItem>
 
-                            <NavItem>
-                                <NavLink className="nav-link" to="/contactus">
-                                    <span className="fa fa-address-card fa-lg">Contant Us</span>
-                                </NavLink>
-                            </NavItem>
-                        </Nav>
+                                <NavItem>
+                                    <NavLink className="nav-link" to="/contactus">
+                                        <span className="fa fa-address-card fa-lg">Contant Us</span>
+                                    </NavLink>
+                                </NavItem>
+                            </Nav>
+                        </Collapse>
                     </div>
                 </Navbar>
                 <div className="jumbotron">
