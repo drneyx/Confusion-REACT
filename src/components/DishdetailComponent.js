9ca7ed448c3a6d749b1c12e827a823c1d2c596eb
Nathani Luving <cosmasnathan9@gmail.com>
2022-10-27 19:35:37 +0300

    Add breadcrumb to dish detail

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import {Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Link } from 'react-router-dom';
+import {Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
 
 
    function  RenderDish({dish}){
@@ -51,8 +52,19 @@ import {Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'rea
         return (
             <div className="container">
                 <div className="row">
-                    <RenderDish dish={props.dish} />
-                    <RenderComments  comment={props.dish.comments} />
+                    <Breadcrumb> 
+                        <BreadcrumbItem><Link to="/home">Home</Link> </BreadcrumbItem>
+                        <BreadcrumbItem><Link to="/menu">Menu</Link> </BreadcrumbItem>
+                        <BreadcrumbItem active>{props.dish.name} </BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>{props.dish.name}</h3>
+                        <hr/>
+                    </div>
+                </div>
+                <div className="row">
+                    <RenderDish dish={props.dish} /> 
+                    <RenderComments  comment={props.comments} />
                 </div>
             </div>
         )
