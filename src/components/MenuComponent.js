43643aa9b558a560a36773c67a06c3ccce4195a7
Nathani Luving <cosmasnathan9@gmail.com>
2022-10-27 14:39:57 +0300

    SWITCH ROUTER

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,16 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import {Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
 
 
 function RenderMenuItem({dish, onClick}) {
     return (
-        <Card key={dish.id}
-            onClick={() => onClick(dish.id)}>
-            <CardImg width="100%" src={dish.image} alt={dish.name}/>
-            
-            <CardImgOverlay>
-                <CardTitle>{dish.name}</CardTitle>
-            </CardImgOverlay>
+        <Card key={dish.id}>
+            <Link to={`/menu/${dish.id}`}>
+                <CardImg width="100%" src={dish.image} alt={dish.name}/>
+                
+                <CardImgOverlay>
+                    <CardTitle>{dish.name}</CardTitle>
+                </CardImgOverlay>
+            </Link>
         </Card>
 
     )
@@ -21,7 +23,7 @@ const Menu = (props) => {
     const menu = props.dishes.map((dish) => {
         return (
             <div key={dish.id} className="col-12 col-md-5 m-1">
-                <RenderMenuItem dish={dish} onClick={props.onClick} />
+                <RenderMenuItem dish={dish}  />
             </div>
         );
     });
