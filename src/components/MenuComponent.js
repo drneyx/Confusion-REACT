47087e9b41877f38f6ace7929928809c35562ed2
Nathani Luving <cosmasnathan9@gmail.com>
2022-11-02 21:30:20 +0300

    Addl to components

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import {Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { baseUrl } from '../shared/baseUrl';
 import { Loading } from './LoadingComponent';
 
 
@@ -8,16 +9,14 @@ function RenderMenuItem({dish, onClick}) {
     return (
         <Card key={dish.id}>
             <Link to={`/menu/${dish.id}`}>
-                <CardImg width="100%" src={dish.image} alt={dish.name}/>
+                <CardImg width="100%" src={baseUrl + dish.image} alt={dish.name}/>
                 
                 <CardImgOverlay>
                     <CardTitle>{dish.name}</CardTitle>
                 </CardImgOverlay>
             </Link>
         </Card>
-
     )
-    
 }
 
 const Menu = (props) => {
