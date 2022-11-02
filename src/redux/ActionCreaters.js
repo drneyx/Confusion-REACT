a9c173e204f644100f72ddeb3571ea39114a740c
Nathani Luving <cosmasnathan9@gmail.com>
2022-11-02 22:52:37 +0300

    Handle more error

--- src/redux/ActionCreaters.js
+++ src/redux/ActionCreaters.js
@@ -20,11 +20,21 @@ export const fetchDishes = () => (dispatch) => {
    return fetch(baseUrl + 'dishes')
           .then(response => {
             if (response.ok){
-                return response
+                return response;
             }
+            else {
+                var error = new Error('Error ' + response.status + ': ' + response.statusText);
+                error.response = response;
+                throw error;
+            }
+          },
+          error => {
+            var errmess = new Error(error.message);
+            throw errmess;
           })
           .then(response => response.json())
-          .then(dishes => dispatch(addDishes(dishes)));
+          .then(dishes => dispatch(addDishes(dishes)))
+          .catch(err => dispatch(dishesFailed(err.message)))
 }
 
 export const dishesLoading = () => ({
