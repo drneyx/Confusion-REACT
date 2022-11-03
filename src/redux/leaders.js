f791a4fa02722065928d16852c4a7af83505ec75
Nathani Luving <cosmasnathan9@gmail.com>
2022-11-03 11:22:19 +0300

    Finish reducer function on leaders

--- src/redux/leaders.js
+++ src/redux/leaders.js
@@ -7,6 +7,14 @@ export const Leaders = (state = {
     leaders: []
 }, action) => {
     switch (action.type) {
+        case ActionTypes.ADD_LEADERS:
+            return {...state, isLoading: false, errMess: null, leaders: action.payload}
+        
+        case ActionTypes.LEADERS_LOADING:
+            return {...state, isLoading: true, errMess: null, leaders: []};
+
+        case ActionTypes.LEADERS_FAILED:
+            return {...state, isLoading: false, errMess: action.payload, leaders: []};
         default:
             return state
     }
