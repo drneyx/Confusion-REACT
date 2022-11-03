bad059c05f580c9241918fb1dcfd8276cc68030a
Nathani Luving <cosmasnathan9@gmail.com>
2022-11-03 11:29:33 +0300

    Add action creators for leaders

--- src/redux/ActionCreaters.js
+++ src/redux/ActionCreaters.js
@@ -167,3 +167,42 @@ export const addPromos = (promos) => ({
     type: ActionTypes.ADD_PROMOS,
     payload: promos,
 });
+
+
+export const fetchLeaders = () => (dispatch) => {
+    dispatch(leadersLoading(true));
+
+   return fetch(baseUrl + 'leaders')
+          .then(response => {
+            if (response.ok){
+                return response;
+            }
+            else {
+                var error = new Error('Error ' + response.status + ': ' + response.statusText);
+                error.response = response;
+                throw error;
+            }
+          },
+          error => {
+            var errmess = new Error(error.message);
+            throw errmess;
+          })
+          .then(response => response.json())
+          .then(leaders => dispatch(addLeaders(leaders)))
+          .catch(err => dispatch(leadersFailed(err.message)))
+}
+
+export const leadersLoading = () => ({
+    type: ActionTypes.LEADERS_LOADING
+});
+
+export const leadersFailed = (errMess) => ({
+    type: ActionTypes.LEADERS_FAILED,
+    payload: errMess
+
+});
+
+export const addLeaders = (leaders) => ({
+    type: ActionTypes.ADD_LEADERS,
+    payload: leaders,
+});
